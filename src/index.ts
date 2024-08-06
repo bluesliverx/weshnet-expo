b1d14ba8b3e6816314e4a72a301629c9c34da163
saville <saville@adobe.com>
2024-08-05 23:39:00 -0600

    More improvements to get types available to external apps and make invoke method work properly

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 // import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
 
-// Import the native module. On web, it will be resolved to WeshnetExpo.web.ts
-// and on native platforms to WeshnetExpo.ts
-import api from './api'
+import api from './api';
 import { rpcBridgeImpl } from './rpc'
 import { createServiceClient } from './service'
 import { ProtocolServiceClient } from './weshnet.types.gen'
-import WeshnetExpoModule from './WeshnetExpoModule'
 import base64 from "base64-js";
+// Import the native module. On web, it will be resolved to WeshnetExpo.web.ts
+// and on native platforms to WeshnetExpo.ts
+import WeshnetExpoModule from './WeshnetExpoModule'
+
+// Export these types to be used in consuming apps
+export * as ProtocolTypes from './api/protocoltypes.pb';
+export * as Weshnet from './weshnet.types.gen';
 
 export async function init(): Promise<ProtocolServiceClient> {
   return WeshnetExpoModule.init()
@@ -18,10 +22,12 @@ export async function init(): Promise<ProtocolServiceClient> {
 }
 
 export async function invokeMethod(method: string, ...args: any): Promise<any> {
-  return WeshnetExpoModule.invokeMethod(method, base64.fromByteArray(new TextEncoder().encode(JSON.stringify(args))))
-    .then((result: any) =>
-      JSON.parse(new TextDecoder().decode(base64.toByteArray(result)))
-    )
+  const encodedArgs = base64.fromByteArray(new TextEncoder().encode(JSON.stringify(args)));
+  return WeshnetExpoModule.invokeMethod(method, encodedArgs)
+    .then((result: any) => {
+      const decodedResult = JSON.parse(new TextDecoder().decode(base64.toByteArray(result)))
+      return decodedResult;
+    })
     .catch((err: any) => console.error('invokeMethod error', err))
 }
 
